0cef4bc753ab9723b68a58823216498c39e77713
allenki_akshay <allenkiakshay8322@gmail.com>
2025-01-09 23:30:02 +0530

    Refactor aboutsectionmobile component and update university information

--- src/components/Homepage/sections/aboutsectionmobile.jsx
+++ src/components/Homepage/sections/aboutsectionmobile.jsx
@@ -40,19 +40,21 @@ export default function AboutMobile() {
         </div>
         <div className="w-full h-full relative flex flex-col gap-2 pb-6">
           <h2 className="text-[24px] text-white text-left font-Emilio font-bold">
-            VITOPIA
+            VIT-AP
           </h2>
           <h1 className="text-white font-Montserrat text-[18px] text-justify">
-            About VITopia '24
+            About VIT-AP
           </h1>
         </div>
         <p className="text-white font-Montserrat text-[14px] text-justify">
-          VITOPIA is the annual international cultural and sports festival
-          conducted by VIT-AP University, celebrating its endless enthusiasm for
-          various sports and art forms. VITOPIA 2024 marks the fifth edition of
-          this cultural and sports extravaganza, scheduled for February 24th and
-          25th, 2024. The sports event features 15+ indoor and outdoor games,
-          while the cultural event includes over 20 prime cultural competitions.
+          Our university achieved the prestigious #1 ranking as the Emerging
+          Private University in India in the 2022 Outlook Ranking and secured a
+          place in the Diamond Band (A+) Category in the 2023 R-World
+          Institutional Ranking. This ranking emphasizes Outcome-Based Education
+          (OBE), showcasing our unwavering commitment to excellence in
+          education. With a staggering 90% placement rate and an impressive
+          average package of 34.4 LPA, we ensure that your academic journey
+          leads to promising career opportunities.
         </p>
       </div>
     </div>
